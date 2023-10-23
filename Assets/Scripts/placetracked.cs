96a7fa3f53f4286d30f53b3df462f494600a60a7
mnba974 <mnba974@gmail.com>
2023-10-23 15:11:20 +0200

    maybe_mvp

--- Assets/Scripts/placetracked.cs
+++ Assets/Scripts/placetracked.cs
@@ -56,11 +56,15 @@ public class PlaceTrackedImages : MonoBehaviour
                     var newPrefab = Instantiate(curPrefab, trackedImage.transform);
                     // Add the created prefab to our array
                     _instantiatedPrefabs[imageName] = newPrefab;
-                    this.enabled = false;
+                    
                 }
             }
         }
-
-
+        foreach (var trackedImage in eventArgs.updated)
+        {
+            var imageName = trackedImage.referenceImage.name;
+            _instantiatedPrefabs[imageName].transform.position = trackedImage.transform.position + new Vector3(0,0,0.5f);
+        }
+        
     }
 }
