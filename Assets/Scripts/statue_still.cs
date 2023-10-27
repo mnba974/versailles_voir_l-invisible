aecc33cce517243d8fe3ff407a5fce18a7a2aee3
mnba974 <mnba974@gmail.com>
2023-10-27 09:27:24 +0200

    27/10

--- Assets/Scripts/statue_still.cs
+++ Assets/Scripts/statue_still.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class statue_still : MonoBehaviour
 {
+    public float l;
+    public GameObject lowestplane;
     // Start is called before the first frame update
     void Start()
     {
-        
+        lowestplane = GameObject.Find("lowplaneposition");
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.rotation = Quaternion.Euler(0, 180, 0);
+        l = lowestplane.GetComponent<lowplane>().lowest;
+        transform.rotation = Quaternion.Euler(0, transform.rotation.eulerAngles.y, 0);
+        transform.position = new Vector3(transform.position.x, l ,transform.position.z);
     }
 }
