5ea12da29444bc8ebe5581e7fbc02ad90a1001a7
mnba974 <mnba974@gmail.com>
2023-10-30 09:04:22 +0100

    achelous + corne_beta

--- Assets/Scripts/canvas_camera.cs
+++ Assets/Scripts/canvas_camera.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class canvas_prefab : MonoBehaviour
+public class canvas_camera : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
